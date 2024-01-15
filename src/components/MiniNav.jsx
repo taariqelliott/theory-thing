400bb340f62bc2fa942d3ba5ce43eb9723bec44b
Taariq Elliott <taariq.elliott@yahoo.com>
2024-01-14 19:41:04 -0800

    added mobile friendly version and changed styling

---
+++ src/components/MiniNav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './MiniNav.css'
+
+const MiniNav = () => {
+    return (
+        <div className='mininav'>
+            <a href="/desktop" style={{ marginRight: '10px' }}>
+                <h4 className='mininav-h4'>Desktop Version</h4>
+            </a>
+
+            <a href="/mobile">
+                <h4 className='mininav-h4'>Mobile Version</h4>
+            </a>
+        </div>
+    )
+}
+
+export default MiniNav
